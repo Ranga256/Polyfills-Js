37cd3c491fd8a263e8459dd748ecd5a8c56660c0
Ranga Sai <rangasai.kakarla@gmail.com>
2023-01-04 16:00:07 +0530

    Push polyfill

---
+++ push.js
@@ -0,0 +1,10 @@
+Array.prototype.mypush = function () {
+    for (let i = 0; i < arguments.length; i++) {
+        this[this.length] = arguments[i];
+    }
+    return this.length;
+}
+
+const months = ['Jan', 'March', 'June','Dec','August'];
+months.push("April","May");
+console.log(months);
