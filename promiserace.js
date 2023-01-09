29a28aa4dfed41e9e32ea882ee715f6a9a6bf995
Ranga256 <51268924+Ranga256@users.noreply.github.com>
2023-01-09 15:44:46 +0530

    Add files via upload

---
+++ promiserace.js
@@ -0,0 +1,39 @@
+Promise.myrace = (arrOfPromises)=> {
+  return new Promise((resolve, reject) => {
+      for (let i = 0; i < arrOfPromises.length; i++) {
+          arrOfPromises[i].then((data) => {
+              resolve(data);
+          }).catch((error) => {
+              reject(error);
+          })
+      }
+  });
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, 'one');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 100, 'two');
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 200, '3rd promise');
+});
+
+Promise.myrace([promise1,promise2]).then((data)=>{
+  console.log(data);
+}).catch((error)=>{
+  console.log(error);
+});
+
+//[ 2, 'Some error' ]
+
+Promise.myrace([promise1,promise3]).then((data)=>{
+  console.log(data);
+}).catch((error)=>{
+  console.log(error);
+});
+
+//[2,'Success]
