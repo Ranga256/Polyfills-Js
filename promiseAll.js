52b26a022f0d95cc58ecfbd83fd3f9899d502058
Ranga256 <51268924+Ranga256@users.noreply.github.com>
2023-01-09 14:45:49 +0530

    Add files via upload

---
+++ promiseAll.js
@@ -0,0 +1,36 @@
+Promise.myall = (arrOfPromises)=> {
+    return new Promise((resolve, reject) => {
+        let result = [];
+        for (let i = 0; i < arrOfPromises.length; i++) {
+            arrOfPromises[i].then((data) => {
+                result.push(data);
+                if (i == arrOfPromises.length - 1) {
+                    resolve(result);
+                }
+            }).catch((error) => {
+                reject(error);
+            })
+        }
+    });
+}
+
+const p1 = Promise.resolve(2);
+const p2 = Promise.reject("Some error");
+
+const p3 = Promise.resolve("Success");
+
+Promise.myall([p1,p2]).then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error);
+});
+
+//some error
+
+Promise.myall([p1,p3]).then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error);
+});
+
+//[2,'Success]
