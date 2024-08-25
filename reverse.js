39061c7282483cc34228d634b9aec92d5f66b635
Ranga256 <51268924+Ranga256@users.noreply.github.com>
2024-08-25 22:37:33 +0530

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,15 +1,17 @@
-Array.prototype.myshift = function() {
+Array.prototype.myReverse = function() {
     let end = this.length;
     if (this.length > 0) {
         for (let i = 0; i < end/2; i++) {
+            let temp = this[i];
             this[i] = this[end-1-i];
+            this[end-1-i] = temp;
         }
     }
     return this;
 }
 
 const months = ['Jan', 'March', 'June','Dec','August'];
-months.reverse();
+months.myReverse();
 console.log(months);
 
 //[ 'August', 'Dec', 'June', 'March', 'Jan' ]
